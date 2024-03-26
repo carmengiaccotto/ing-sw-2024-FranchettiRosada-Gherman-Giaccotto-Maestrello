e5e11d51bc0b8f7c9b37e2232c4d6cf519919821
denisagherman19 <denisaminodora.gherman@mail.polimi.it>
2024-03-26 12:54:59 +0100

    Javadoc on PlayArea

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/PlayArea.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/PlayArea.java
@@ -2,25 +2,54 @@ package CodexNaturalis.src.main.java.it.polimi.ingsw.model;
 import it.polimi.ingsw.model.Card;
 
 import java.util.Map;
-
+/*@author Denisa Minodora Gherman
+* Class that implements the PlayArea of the single Player. It is built as a Matrix whose Type is Card:
+* The player has already chosen the side they want to play the Card on*/
 public class PlayArea {
+    /*Number of occurrences of each symbol on the PlayArea. Needed for objective Cards and for goldCards requirement to be placed*/
     private Map<Symbol, Integer > Symbols;
     private Card[][] CardsOnArea;
 
+
+
+
+
+    /*CardsOnArea matrix getter method
+    * @return Card[][] which represents the PlayArea to which the cards are to be added at every Round*/
     public Card[][] getCardsOnArea() {
         return CardsOnArea;
     }
+
+
+
+
+
+    /*CardsOnArea setter method
+    * @return Card[][]
+    *
+    * Do we need this??
     public void setCardsOnArea(Card[][] cardsOnArea) {
         CardsOnArea = cardsOnArea;
     }
 
+     */
+
+
+
+    /*This method returns the card placed in a certain position in the PlayArea
+    * @return Card
+    * @param int row
+    * @param int column
+    * @throws NullPointer exception if the PlayArea does not Exist yet
+    * @throws ArrayIndexOutOfBoundsException if we are trying to search an invalid position
+    * row and column represent the position we want to search*/
     public Card getCard( int row, int column) {
         if (CardsOnArea != null) {
             if (row < CardsOnArea.length && column<CardsOnArea[0].length && row>=0 && column>=0) {
                 return CardsOnArea[row][column];
             }
             else {
-                throw new NullPointerException("This position does not exist on your playArea");
+                throw new ArrayIndexOutOfBoundsException("This position does not exist on your playArea");
             }
 
         }
@@ -28,6 +57,11 @@ public class PlayArea {
             throw new NullPointerException("The PlayArea is non existent");
         }
     }
+
+
+
+
+
 
     public Map<Symbol, Integer> getSymbols() {
         return Symbols;
