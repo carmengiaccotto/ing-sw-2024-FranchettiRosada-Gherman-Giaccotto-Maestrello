1340ed68f6df405cb9dde21d6c31b0606fa8ce6d
denisagherman19 <denisaminodora.gherman@mail.polimi.it>
2024-03-26 12:23:11 +0100

    Javadoc and comments on Player

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Player.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Player.java
@@ -6,7 +6,7 @@ import it.polimi.ingsw.model.Card;
 /*@author Denisa Minodora Gherman
 * Class that represents the Player of the game which coincides with the client*/
 public class Player {
-    /*Every Player has its own playArea. All t*/
+    /*Every Player has its own playArea.*/
     private PlayArea playArea;
     private String nickname;
     private Color pawnColor;
@@ -22,54 +22,157 @@ public class Player {
         this.cardsInHand = new ArrayList<>(); //Play Ground initializer will add the elements to this list for the first round
         this.round = 0;
     }
-
+    /*getter method for the Player's playArea
+    * @return PlayaArea*/
     public PlayArea getPlayArea() {
         return playArea;
     }
+
+
+
+
+    /*playArea setter Method
+    * @param PlayArea*/
     public void setPlayArea(PlayArea playArea) {
         this.playArea = playArea;
     }
+
+
+
+
+    /*getter method for the Player's Nickname
+     * @return string NickName*/
 
     public String getNickname() {
         return nickname;
     }
 
+
+
+
+
+
+
+    /*Nickname setter Method
+     * @param nickname*/
     public void setNickname(String nickname) {
         this.nickname = nickname;
     }
 
+
+
+
+
+    /*pawnColor getter method
+    * @return Color*/
     public Color getPawnColor() {
         return pawnColor;
     }
 
+
+
+
+
+
+    /*pawnColor setter Method
+     * @param pawnColor the type is the enumeration Color*/
     public void setPawnColor(Color pawnColor) {
         this.pawnColor = pawnColor;
     }
 
+
+
+
+    // here or in the playground?
+    /*public Color ChoosepawnColor(){
+        for (Player: Playground.getPlayers()){}
+
+        return pawnColor;
+    }*/
+
+
+
+
+    /*Score getter Method
+    * @return int Score*/
     public int getScore() {
         return score;
     }
 
+
+
+
+
+
+    /*Score setter Method
+    * @param score the current score of the player*/
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+
+
+
+
+
+    /*Increases the score of the Player
+    * @param points
+    * points are the additional points that the player just gained, either during the match
+    *  (Resource or Gold Cards) or at the end of the game, when the objective Cards points are to be added */
     public void increaseScore(int points) {
         score += points;
     }
 
-    public void setScore(int score) {
-        this.score = score;
-    }
+
+
+
+
+    /*Round getter Method
+    * @return round number of the round the Player already Played*/
     public int getRound() {
         return round;
     }
 
+
+
+
+
+
+
+    /*Cards getter. The cards are still declared as PairOfCards cards since the player has them in hand, so
+    * he didn't play them yet and hasn't decided which side they want to play
+    * @return ArrayList<PairOfCards> cards that the player can currently play with */
     public ArrayList<PairOfCards> getCardsInHand() {
         return cardsInHand;
     }
 
-    public void drawCardFromDeck(Deck deck) { //TypeOfCards è una enum dei possibili mazzi che ci sono
+
+
+
+
+
+
+    /*When the player placed their card on the PlayArea, they have to draw a new one.
+    * If they decided to draw from deck, they have to choose if they want a Gold Card or a Resource Card
+    * @param deck the deck they want to draw from
+    * There are going to be two instances of deck, GoldDeck and ResourceDeck.*/
+    public void drawCardFromDeck(Deck deck) {
         cardsInHand.add(deck.DrawCard());
     }
+
+
+
+
+
+
+    /*Used If the Players wants to draw a Card from the common Cards on the playGround
+    * @param Card PairOfCards type
+    * Once the player added the card to their hand, it has to be removed from the playground
+    * A new card of the same type needs to be drawn. We get the type of the drawn Card, and we use it as TypeOfDeck to
+    * search for*/
     public void drawCardFromPlayground(PairOfCards card) {
         cardsInHand.add(card);
+        commonCards.remove(card);
         String cardType = card.getClass().getSimpleName();
         for (Deck deck : Playground.getDecks) {
             if (deck.getTypeOfDeck().equals(cardType))
@@ -86,9 +189,23 @@ public class Player {
     }*/
     //This method is probably going to be implemented in PairOfCards class
 
-    /*In this class the player plays pairOfCard; PairOfCard is going to implement the method in the previous comment to choose the side*/
+
+
+
+
+
+
+
+
+
+    /*In this class the player plays pairOfCard; PairOfCard is going to implement the method in the previous comment to choose the side and assign it to Card
+    * param CardToPlay
+    * this is the last time the card gets used as PaiOfCards: from now on it is going to exist in the game just as the side it was played*/
     public void playCard(PairOfCards cardToPlay) {
         cardToPlay.ChooseSide();
     }
 }
+
+
+
 
