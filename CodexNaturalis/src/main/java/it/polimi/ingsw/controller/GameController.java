c017da887eb9fd4ab5c81237b1d5dfac4d6c12c6
carmengiaccotto <carmen.giaccotto@mail.polimi.it>
2024-04-28 01:32:48 +0200

    GameController structure and GameStatus enumeration

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -0,0 +1,103 @@
+package CodexNaturalis.src.main.java.it.polimi.ingsw.controller;
+
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.PlayGround.Player;
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Enumerations.GameStatus;
+
+import java.util.List;
+
+public class GameController implements Runnable {
+
+    private Playground model;
+
+    @Override
+    public void run() {
+
+    }
+
+    /** Add a player to Game's lobby. The player will be asked to set a nickname */
+    public void addPlayerToLobby(Player p) throws MaxNumPlayersReached {
+    }
+
+    /** Check if the nickname is valid and unique*/
+    public void checkUniqueNickname(){
+
+    }
+
+    /** Return a list containing all the current players of the game (Online and Offline)*/
+    public List <Player> getPlayers() {
+        return model.getPlayers();
+    }
+
+    public int getNumOfPlayers() {
+        return model.getNumOfPlayers();
+    }
+
+    /** Reconnect a player to the Game unless the game is already over*/
+    public void reconnectPlayer(Player p) throws  GameEndedException {
+    }
+
+    private void extractCommonObjectiveCards() {
+    }
+
+    private void extractCommonPlaygroundCards() {
+
+    }
+
+
+
+    private void extractFirstPlayer() {
+
+    }
+
+    public synchronized void sentMessage() {
+    }
+
+    private void checkObjectivePoints() {
+
+    }
+
+    public void executePlayerTurn() {
+    }
+
+
+    public int getGameId() {
+        return model.getGameId();
+    }
+
+
+    public void FinalizeGame(){
+
+    }
+
+    public void decreeWinner(){
+
+    }
+
+    /**
+     * Return the GameStatus of the model
+     *
+     * @return status
+     */
+    public GameStatus getStatus() {
+        return model.getStatus();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Enumerations/GameStatus.java
@@ -0,0 +1,8 @@
+package CodexNaturalis.src.main.java.it.polimi.ingsw.model.Enumerations;
+
+public enum GameStatus {
+    WAIT,
+    RUNNING,
+    LAST_TURN,
+    ENDED,
+}
