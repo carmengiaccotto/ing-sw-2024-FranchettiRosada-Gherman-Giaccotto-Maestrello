543329222ed20b29b766e858ef2165c4c0fc7a58
denisagherman19 <denisaminodora.gherman@mail.polimi.it>
2024-04-27 20:24:45 +0200

    ObjectivePoints Tests. Enum Tests finished

--- CodexNaturalis/src/test/java/it/polimi/ingsw/ObjectivePointsTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/ObjectivePointsTest.java
@@ -1,14 +1,45 @@
 package CodexNaturalis.src.test.java.it.polimi.ingsw;
 
 import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Enumerations.ObjectivePoints;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class ObjectivePointsTest {
     @Test
-    public void testEnumValues() {
-        Assertions.assertEquals(2, ObjectivePoints.TWO.getValue());
-        Assertions.assertEquals(3, ObjectivePoints.THREE.getValue());
+    public void testDefinedValues() {
+        assertNotNull(ObjectivePoints.TWO);
+        assertNotNull(ObjectivePoints.THREE);
+        assertEquals(2, ObjectivePoints.TWO.getValue());
+        assertEquals(3, ObjectivePoints.THREE.getValue());
+    }
+
+    @Test
+    public void testEquality() {
+        assertEquals(ObjectivePoints.TWO, ObjectivePoints.TWO);
+        assertEquals(ObjectivePoints.THREE, ObjectivePoints.THREE);
+        assertNotEquals(ObjectivePoints.TWO, ObjectivePoints.THREE);
+    }
+
+    @Test
+    public void testConversions() {
+        assertEquals(2, ObjectivePoints.TWO.getValue());
+        assertEquals(ObjectivePoints.TWO, ObjectivePoints.valueOf("TWO"));
+    }
+
+    @Test
+    public void testSwitchStatement() {
+        ObjectivePoints points = ObjectivePoints.THREE;
+        switch(points) {
+            case TWO:
+                assertEquals(2, points.getValue());
+                break;
+            case THREE:
+                assertEquals(3, points.getValue());
+                break;
+            default:
+                fail("Unexpected points");
+        }
     }
 
 }
