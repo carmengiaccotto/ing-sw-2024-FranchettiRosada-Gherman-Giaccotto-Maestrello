5102f5689f71c8b20bcd4c4861d04c10fb39e0a9
denisagherman19 <denisaminodora.gherman@mail.polimi.it>
2024-04-27 20:17:06 +0200

    PawnColorTest all tests passed

---
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/PawnColorTest.java
@@ -0,0 +1,50 @@
+package CodexNaturalis.src.test.java.it.polimi.ingsw;
+
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Enumerations.PawnColor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class PawnColorTest {
+    @Test
+    public void testDefinedValues() {
+        assertNotNull(PawnColor.RED);
+        assertNotNull(PawnColor.GREEN);
+        assertNotNull(PawnColor.YELLOW);
+        assertNotNull(PawnColor.BLUE);
+    }
+    @Test
+    public void testEquality() {
+        assertEquals(PawnColor.RED, PawnColor.RED);
+        Assertions.assertNotEquals(PawnColor.RED, PawnColor.GREEN);
+    }
+
+    @Test
+    public void testSwitchStatement() {
+        PawnColor color = PawnColor.GREEN;
+        switch(color) {
+            case RED:
+                break;
+            case GREEN:
+                assertEquals(PawnColor.GREEN, color);
+                break;
+            case YELLOW:
+                break;
+            case BLUE:
+                break;
+            default:
+                throw new AssertionError("Unexpected color");
+        }
+    }
+
+    @Test
+    public void testStringRepresentation() {
+        assertEquals("RED", PawnColor.RED.toString());
+        assertEquals("GREEN", PawnColor.GREEN.toString());
+        assertEquals("YELLOW", PawnColor.YELLOW.toString());
+        assertEquals("BLUE", PawnColor.BLUE.toString());
+    }
+
+}
