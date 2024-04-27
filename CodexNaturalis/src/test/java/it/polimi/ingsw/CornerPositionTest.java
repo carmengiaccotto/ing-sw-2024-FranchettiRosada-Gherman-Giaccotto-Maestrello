af966c010f3efe425bf75f8068b44439d8b87c74
denisagherman19 <denisaminodora.gherman@mail.polimi.it>
2024-04-27 20:00:54 +0200

    UpDownPosition Test

---
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/CornerPositionTest.java
@@ -0,0 +1,103 @@
+package CodexNaturalis.src.test.java.it.polimi.ingsw;
+
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Cards.Corner;
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Cards.SideOfCard;
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Enumerations.CardColors;
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Enumerations.CornerPosition;
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Pair;
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.PlayGround.PlayArea;
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Symbol;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CornerPositionTest {
+    private static List<List<SideOfCard>> cardsOnArea;
+
+    public static void setCardsOnArea(List<List<SideOfCard>> cardsOnArea) {
+        CornerPositionTest.cardsOnArea = cardsOnArea;
+    }
+
+    private static Corner[][] generateCorners(int rows, int cols) {
+        Corner[][] corners = new Corner[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                corners[i][j] = new Corner(Symbol.FUNGI, false);
+            }
+        }
+        return corners;
+    }
+
+
+    @Test
+    public void TestCoverCorner(){
+        assertEquals(CornerPosition.BOTTOMRIGHT,CornerPosition.TOPLEFT.CoverCorners() );
+        assertEquals(CornerPosition.TOPRIGHT,CornerPosition.BOTTOMLEFT.CoverCorners() );
+        assertEquals(CornerPosition.TOPLEFT,CornerPosition.BOTTOMRIGHT.CoverCorners() );
+        assertEquals(CornerPosition.BOTTOMLEFT,CornerPosition.TOPRIGHT.CoverCorners() );
+    }
+
+    @Test
+    public void TestPositionNewCard(){
+        SideOfCard testCard= new SideOfCard(null, CardColors.RED, null);
+        testCard.setPositionOnArea(new Pair<>(1,3));
+        assertEquals(new Pair<>(0,2), CornerPosition.TOPLEFT.PositionNewCard(testCard));
+        assertEquals(new Pair<>(0,4), CornerPosition.TOPRIGHT.PositionNewCard(testCard));
+        assertEquals(new Pair<>(2,2), CornerPosition.BOTTOMLEFT.PositionNewCard(testCard));
+        assertEquals(new Pair<>(2,4), CornerPosition.BOTTOMRIGHT.PositionNewCard(testCard));
+
+    }
+
+    @BeforeAll
+    public static void CreateTestPlayArea(){
+        List<List<SideOfCard>> cardsOnArea= new ArrayList<>();
+        List<SideOfCard> row1 = new ArrayList<>();
+        SideOfCard card1= new SideOfCard(null,CardColors.BLUE,generateCorners(2,2));
+        row1.add(card1);
+        SideOfCard card2= new SideOfCard(null,CardColors.GREEN,generateCorners(2,2));
+        row1.add(card2);
+        SideOfCard card3= new SideOfCard(null,CardColors.RED,generateCorners(2,2));
+        row1.add(card3);
+        cardsOnArea.add(row1);
+        List<SideOfCard> row2 = new ArrayList<>();
+        SideOfCard card4= new SideOfCard(null,CardColors.GREEN,generateCorners(2,2));
+        row2.add(card4);
+        SideOfCard card5= new SideOfCard(null,CardColors.BLUE,generateCorners(2,2));
+        row2.add(card5);
+        SideOfCard card6= new SideOfCard(null,CardColors.GREEN,generateCorners(2,2));
+        row2.add(card6);
+        cardsOnArea.add(row2);
+        List<SideOfCard> row3 = new ArrayList<>();
+        SideOfCard card7= new SideOfCard(null,CardColors.RED,generateCorners(2,2));
+        row3.add(card7);
+        SideOfCard card8= new SideOfCard(null,CardColors.BLUE,generateCorners(2,2));
+        row3.add(card8);
+        SideOfCard card9= new SideOfCard(null,CardColors.PURPLE,generateCorners(2,2));
+        row3.add(card9);
+        cardsOnArea.add(row3);
+        card5.getCornerInPosition(CornerPosition.TOPLEFT).setNextCorner(card1.getCornerInPosition(CornerPosition.BOTTOMRIGHT));
+        card5.getCornerInPosition(CornerPosition.TOPRIGHT).setNextCorner(card3.getCornerInPosition(CornerPosition.BOTTOMLEFT));
+        card5.getCornerInPosition(CornerPosition.BOTTOMLEFT).setNextCorner(card7.getCornerInPosition(CornerPosition.TOPRIGHT));
+        card5.getCornerInPosition(CornerPosition.BOTTOMRIGHT).setNextCorner(card9.getCornerInPosition(CornerPosition.TOPLEFT));
+        setCardsOnArea(cardsOnArea);
+    }
+
+    @Test
+    void testGetNeighbourCard() {
+        PlayArea testPlayArea=new PlayArea(cardsOnArea);
+        Assertions.assertEquals(cardsOnArea.get(2).get(2), CornerPosition.BOTTOMRIGHT.getNeighbourCard(cardsOnArea.get(1).get(1), testPlayArea));
+        Assertions.assertEquals(cardsOnArea.get(2).get(0), CornerPosition.BOTTOMLEFT.getNeighbourCard(cardsOnArea.get(1).get(1), testPlayArea));
+        Assertions.assertEquals(cardsOnArea.get(0).get(2), CornerPosition.TOPRIGHT.getNeighbourCard(cardsOnArea.get(1).get(1), testPlayArea));
+        Assertions.assertEquals(cardsOnArea.get(0).get(0), CornerPosition.TOPLEFT.getNeighbourCard(cardsOnArea.get(1).get(1), testPlayArea));
+
+
+
+
+    }
+
+}
---
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/UpDownPositionTest.java
@@ -0,0 +1,67 @@
+package CodexNaturalis.src.test.java.it.polimi.ingsw;
+
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Cards.Corner;
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Cards.SideOfCard;
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Enumerations.CardColors;
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Enumerations.UpDownPosition;
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Pair;
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.PlayGround.PlayArea;
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Symbol;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class UpDownPositionTest {
+    private static List<List<SideOfCard>> cardsOnArea;
+    public static void setCardsOnArea(List<List<SideOfCard>> cardsOnArea) {
+        UpDownPositionTest.cardsOnArea = cardsOnArea;
+    }
+
+
+
+    private static Corner[][] generateCorners() {
+        Corner[][] corners = new Corner[2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                corners[i][j] = new Corner(Symbol.FUNGI, false);
+            }
+        }
+        return corners;
+    }
+    @BeforeAll
+    public static void CreateTestPlayArea(){
+        List<List<SideOfCard>> cardsOnArea= new ArrayList<>();
+        List<SideOfCard> row1 = new ArrayList<>();
+        SideOfCard card1= new SideOfCard(null, CardColors.BLUE,generateCorners());
+        card1.setPositionOnArea(new Pair<>(0, 0));
+        row1.add(card1);
+        cardsOnArea.add(row1);
+        List<SideOfCard> row2 = new ArrayList<>();
+        SideOfCard card2= new SideOfCard(null,CardColors.GREEN,generateCorners());
+        card2.setPositionOnArea(new Pair<>(1, 0));
+        row2.add(card2);
+        cardsOnArea.add(row2);
+        List<SideOfCard> row3 = new ArrayList<>();
+        SideOfCard card3= new SideOfCard(null,CardColors.RED,generateCorners());
+        card3.setPositionOnArea(new Pair<>(2, 0));
+        row3.add(card3);
+        cardsOnArea.add(row3);
+        setCardsOnArea(cardsOnArea);
+    }
+
+    @Test
+    public void TestGetNeighbourCard(){
+        PlayArea testPlayArea=new PlayArea(cardsOnArea);
+
+        Assertions.assertEquals(cardsOnArea.get(0).getFirst(), UpDownPosition.UP.getNeighbourCard(cardsOnArea.get(2).getFirst(), testPlayArea));
+        Assertions.assertEquals(cardsOnArea.get(2).getFirst(), UpDownPosition.DOWN.getNeighbourCard(cardsOnArea.get(0).getFirst(), testPlayArea));
+
+
+    }
+
+
+
+}
