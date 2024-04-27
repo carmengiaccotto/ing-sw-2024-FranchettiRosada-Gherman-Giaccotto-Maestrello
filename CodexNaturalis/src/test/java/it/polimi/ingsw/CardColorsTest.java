58f51d05bf57acc12f82690f7264f37746c23859
denisagherman19 <denisaminodora.gherman@mail.polimi.it>
2024-04-27 20:10:20 +0200

    CardColorsTest

---
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/CardColorsTest.java
@@ -0,0 +1,50 @@
+package CodexNaturalis.src.test.java.it.polimi.ingsw;
+
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Enumerations.CardColors;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class CardColorsTest {
+    @Test
+    public void testDefinedValues() {
+        assertNotNull(CardColors.RED);
+        assertNotNull(CardColors.GREEN);
+        assertNotNull(CardColors.PURPLE);
+        assertNotNull(CardColors.BLUE);
+    }
+    @Test
+    public void testEquality() {
+        assertEquals(CardColors.RED, CardColors.RED);
+        Assertions.assertNotEquals(CardColors.RED, CardColors.GREEN);
+    }
+
+    @Test
+    public void testSwitchStatement() {
+        CardColors color = CardColors.GREEN;
+        switch(color) {
+            case RED:
+                break;
+            case GREEN:
+                assertEquals(CardColors.GREEN, color);
+                break;
+            case PURPLE:
+                break;
+            case BLUE:
+                break;
+            default:
+                throw new AssertionError("Unexpected color");
+        }
+    }
+
+    @Test
+    public void testStringRepresentation() {
+        assertEquals("RED", CardColors.RED.toString());
+        assertEquals("GREEN", CardColors.GREEN.toString());
+        assertEquals("PURPLE", CardColors.PURPLE.toString());
+        assertEquals("BLUE", CardColors.BLUE.toString());
+    }
+
+}
