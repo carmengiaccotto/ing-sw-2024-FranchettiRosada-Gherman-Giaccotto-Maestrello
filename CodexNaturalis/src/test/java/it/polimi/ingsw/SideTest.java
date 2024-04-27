6524b056fdfd980d58d4fb670169f0bf006dfd46
denisagherman19 <denisaminodora.gherman@mail.polimi.it>
2024-04-27 20:21:04 +0200

    Side enum Test

---
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/SideTest.java
@@ -0,0 +1,44 @@
+package CodexNaturalis.src.test.java.it.polimi.ingsw;
+
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Enumerations.Side;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SideTest {
+    @Test
+    public void testDefinedValues() {
+        assertNotNull(Side.FRONT);
+        assertNotNull(Side.BACK);
+        assertNotEquals(Side.FRONT, Side.BACK);
+    }
+
+    @Test
+    public void testEquality() {
+        assertEquals(Side.FRONT, Side.FRONT);
+        assertEquals(Side.BACK, Side.BACK);
+        assertNotEquals(Side.FRONT, Side.BACK);
+    }
+
+    @Test
+    public void testSwitchStatement() {
+        Side side = Side.BACK;
+        switch(side) {
+            case FRONT:
+                assertEquals(Side.FRONT, side);
+                break;
+            case BACK:
+                assertEquals(Side.BACK, side);
+                break;
+            default:
+                fail("Unexpected side");
+        }
+    }
+
+    @Test
+    public void testStringRepresentation() {
+        assertEquals("FRONT", Side.FRONT.toString());
+        assertEquals("BACK", Side.BACK.toString());
+    }
+
+}
