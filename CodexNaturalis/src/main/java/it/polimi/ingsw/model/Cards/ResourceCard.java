264896e49e49e1e1eb50af89aeeb5593981820b0
denisagherman19 <denisaminodora.gherman@mail.polimi.it>
2024-04-28 20:15:13 +0200

    ResourceCard Test and modified Resource Card

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Cards/ResourceCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Cards/ResourceCard.java
@@ -1,27 +1,35 @@
 package CodexNaturalis.src.main.java.it.polimi.ingsw.model.Cards;
 
 import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Enumerations.CardColors;
-import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Symbol;
+import CodexNaturalis.src.main.java.it.polimi.ingsw.model.Enumerations.Side;
 import com.google.gson.JsonObject;
-
-import java.util.HashMap;
 
 
 /** This subclass represents the Resource Cards and extends the SideOfCard class*/
-public class ResourceCard extends SideOfCard {
-    private boolean point;
+public class ResourceCard extends PlayCard {
+    private final boolean FrontPoint;
 
-    /**Class Constructor*/
-    public ResourceCard(HashMap<Symbol, Integer> symbols, CardColors color, Corner[][] corners, boolean point) {
-        super(symbols, null, corners);
-        this.point=point;
+
+    /**
+     * Class Constructor
+     */
+
+    public ResourceCard(int id, SideOfCard front, SideOfCard back, CardColors color, boolean FrontPoint) {
+        super(id, front, back, color);
+        this.FrontPoint = FrontPoint;
 
     }
 
+
+
+
     public ResourceCard mapFromJson(JsonObject jsonObject) {
-        SideOfCard sideOfCard = super.mapSideFromJson(jsonObject);
-        this.point=jsonObject.get("point").getAsBoolean();
-        return new ResourceCard(sideOfCard.getSymbols(), sideOfCard.getColor(), sideOfCard.getCorners(),this.point );
+        PlayCard playCard = super.mapFromJson(jsonObject);
+        JsonObject FrontCard = jsonObject.getAsJsonObject("front");
+        boolean FrontPoint = FrontCard.get("point").getAsBoolean();
+        return new ResourceCard(playCard.getIdCard(), playCard.getFront(), playCard.getBack(), playCard.getColor(), FrontPoint);
+
+
     }
 
 
@@ -30,7 +38,16 @@ public class ResourceCard extends SideOfCard {
      *
      * @return point
      */
-    public boolean getPoint(){
-        return point;
+    public boolean getPoint(Side chosenSide) {
+        if (chosenSide.equals(Side.FRONT))
+            return FrontPoint;
+        if (chosenSide.equals(Side.BACK))
+            return false;
+        else
+            throw new IllegalArgumentException("Invalid Side Chosen");
     }
+
 }
+
+
+
