6c4480228c0149fc4b4e94697d21f77c86f834b2
denisagherman19 <denisaminodora.gherman@mail.polimi.it>
2024-05-06 19:08:58 +0200

    Deleted Classes

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Connection/ClientConnectionHandler.java
+++
@@ -1,9 +0,0 @@
-package CodexNaturalis.src.main.java.it.polimi.ingsw.Connection;
-
-public interface ClientConnectionHandler {
-    void connect();
-    void disconnect();
-
-
-
-}
--- CodexNaturalis/src/main/java/it/polimi/ingsw/Server/LobbyMessage.java
+++
@@ -1,7 +0,0 @@
-package CodexNaturalis.src.main.java.it.polimi.ingsw.Server;
-
-public enum LobbyMessage {
-    NUMBER_OF_PLAYERS,
-    SELECT_NICKNAME,
-    SELECT_PAWN_COLOR
-}
