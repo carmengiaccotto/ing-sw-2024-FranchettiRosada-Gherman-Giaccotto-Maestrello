be44c0bf60f08b02494844a1f1bd066898ed8280
denisagherman19 <denisaminodora.gherman@mail.polimi.it>
2024-06-01 00:16:21 +0200

    Solved some issues in module-info.java

--- CodexNaturalis/src/main/java/module-info.java
+++ CodexNaturalis/src/main/java/module-info.java
@@ -4,7 +4,11 @@ module it.polimi.ingsw {
     requires com.google.gson;
     requires java.rmi;
     requires java.desktop;
+    requires jdk.unsupported.desktop;
     exports it.polimi.ingsw.View;
+    exports it.polimi.ingsw.Controller.Main to java.rmi;
+    exports it.polimi.ingsw.Controller.Client to java.rmi;
+    exports it.polimi.ingsw.Controller.Game to java.rmi;
     opens it.polimi.ingsw.View to javafx.fxml, javafx.graphics;
     opens it.polimi.ingsw.Model to com.google.gson;
 }
