1edc4dd990ccfa1ae91e87d7160ff277a65b10d7
alessiafranchettirosada <alessia.franchettirosada@mail.polimi.it>
2024-06-27 18:35:22 +0200

    JavaDoc completed for the Model PlayGround.

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Model/PlayGround/EdgePositions.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Model/PlayGround/EdgePositions.java
@@ -6,18 +6,36 @@ import it.polimi.ingsw.Model.Pair;
 import java.util.ArrayList;
 import java.util.List;
 
-
-/**EdgePositions Class:
+/**
+ * EdgePositions Class:
  * Purpose: Managing the Edge Cases the Play Area can be in, when the Player is Placing a Card:
  *          Checking if the Player's move needs the PlayArea dimensions to be modified
  *          Modify the PlayArea dimensions if needed
  * Contains: EdgeCases ENUM to keep track of all the only possible edge cases
  *          SetCornersToCheck method
- *          */
+ */
 
 public class EdgePositions {
+
+    /**
+     * The EdgeCases enum represents the possible edge cases that the play area can be in.
+     * Each enum value represents a different edge case and overrides the ExpandArea and isEdgePosition methods to handle that specific case.
+     */
     public enum EdgeCases {
+
+        /**
+         * The RowZero enum value represents the case where the player wants to place a card in the first row.
+         * It overrides the ExpandArea method to add a new row at the top of the play area and the isEdgePosition method to check if the card is in the first row.
+         */
         RowZero{
+
+            /**
+             * This method is used to expand the play area by adding a new row at the top.
+             * It creates a new ArrayList and fills it with null values, representing empty spaces.
+             * Then, it adds this new row to the top of the play area.
+             *
+             * @param cardsOnArea The current state of the play area.
+             */
             @Override
             public void ExpandArea(List<List<SideOfCard>> cardsOnArea){
                 ArrayList<SideOfCard> array = new ArrayList<>();
@@ -26,27 +44,65 @@ public class EdgePositions {
                 cardsOnArea.add(0, array);
             }
 
+            /**
+             * This method is used to check if the card is being placed in the first row of the play area.
+             *
+             * @param occupiedPosition The position where the player wants to place the card.
+             * @param cardsOnArea The current state of the play area.
+             * @return True if the card is being placed in the first row, false otherwise.
+             */
             @Override
             public boolean isEdgePosition(Pair<Integer, Integer> occupiedPosition, List<List<SideOfCard>> cardsOnArea){
                 return occupiedPosition.getFirst()==0; //check if the player wants to place the card in the first row
             }
-
-
         },
+
+        /**
+         * The ColumnZero enum value represents the case where the player wants to place a card in the first column.
+         * It overrides the ExpandArea method to add a new column at the left of the play area and the isEdgePosition method to check if the card is in the first column.
+         */
         ColumnZero{
+
+            /**
+             * This method is used to expand the play area by adding a new column at the left.
+             * It iterates over each row in the play area and adds a null value at the beginning, representing an empty space.
+             *
+             * @param cardsOnArea The current state of the play area.
+             */
             @Override
             public void ExpandArea(List<List<SideOfCard>> cardsOnArea) {
                 for (List<SideOfCard> row : cardsOnArea) {
                     row.add(0, null);}
 
             }
+
+            /**
+             * This method is used to check if the card is being placed in the first column of the play area.
+             *
+             * @param occupiedPosition The position where the player wants to place the card.
+             * @param cardsOnArea The current state of the play area.
+             * @return True if the card is being placed in the first column, false otherwise.
+             */
             @Override
             public boolean isEdgePosition(Pair<Integer, Integer> occupiedPosition, List<List<SideOfCard>> cardsOnArea){
                 return occupiedPosition.getSecond()==0;
             }
 
         },
+
+        /**
+         * The RowMax enum value represents the case where the player wants to place a card in the last row.
+         * It overrides the ExpandArea method to add a new row at the bottom of the play area and the isEdgePosition method to check if the card is in the last row.
+         */
         RowMax{
+
+            /**
+             * This method is used to expand the play area by adding a new row at the bottom.
+             * It creates a new ArrayList and fills it with null values, representing empty spaces.
+             * Then, it adds this new row to the bottom of the play area.
+             *
+             * @param cardsOnArea The current state of the play area.
+             */
             @Override
             public void ExpandArea(List<List<SideOfCard>> cardsOnArea){
                 List<SideOfCard> newRow = new ArrayList<>();
@@ -56,19 +112,47 @@ public class EdgePositions {
                 }
                 cardsOnArea.add(newRow);
             }
+
+            /**
+             * This method is used to check if the card is being placed in the last row of the play area.
+             *
+             * @param occupiedPosition The position where the player wants to place the card.
+             * @param cardsOnArea The current state of the play area.
+             * @return True if the card is being placed in the last row, false otherwise.
+             */
             @Override
             public boolean isEdgePosition(Pair<Integer, Integer> occupiedPosition, List<List<SideOfCard>> cardsOnArea){
                 return occupiedPosition.getFirst()==(cardsOnArea.size()-1);
             }
 
         },
+
+        /**
+         * The ColumnMax enum value represents the case where the player wants to place a card in the last column.
+         * It overrides the ExpandArea method to add a new column at the right of the play area and the isEdgePosition method to check if the card is in the last column.
+         */
         ColumnMax{
+
+            /**
+             * This method is used to expand the play area by adding a new column at the right.
+             * It iterates over each row in the play area and adds a null value, representing an empty space.
+             *
+             * @param cardsOnArea The current state of the play area.
+             */
             @Override
             public void ExpandArea(List<List<SideOfCard>> cardsOnArea){
                 for (List<SideOfCard> row : cardsOnArea) {
                     row.add(null);
                 }
             }
+
+            /**
+             * This method is used to check if the card is being placed in the last column of the play area.
+             *
+             * @param occupiedPosition The position where the player wants to place the card.
+             * @param cardsOnArea The current state of the play area.
+             * @return True if the card is being placed in the last column, false otherwise.
+             */
             @Override
             public boolean isEdgePosition(Pair<Integer, Integer> occupiedPosition,List<List<SideOfCard>> cardsOnArea){
                 return occupiedPosition.getSecond()==(cardsOnArea.getFirst().size()-1);
@@ -76,29 +160,25 @@ public class EdgePositions {
 
         };
 
-
-
-
-
-        /**Method that adds a new Row or a new Column where needed.
-         * Override by the elements of the enum
-         * @param cardsOnArea the PlayArea whose dimensions we need to modify*/
+        /**
+         * This method is used to expand the play area by adding a new row or column where needed.
+         * It is overridden by the elements of the enum.
+         *
+         * @param cardsOnArea The current state of the play area.
+         */
         public void ExpandArea(List<List<SideOfCard>> cardsOnArea) {
         }
 
-
-
-
-        /**Method that checks if the Card is a "Border Card" of the PlayArea.
-         * Override by the elements of the enum
-         * @param cardsOnArea the PlayArea we are checking
-         * @param occupiedPosition position where the player wants to place the card
-         * @return result :true if the position of the card matches the condition that the enum elements impose, false otherwise */
+        /**
+         * This method is used to check if the card is a "Border Card" of the PlayArea.
+         * It is overridden by the elements of the enum.
+         *
+         * @param cardsOnArea The current state of the play area.
+         * @param occupiedPosition The position where the player wants to place the card.
+         * @return True if the position of the card matches the condition that the enum elements impose, false otherwise.
+         */
         public boolean isEdgePosition(Pair<Integer, Integer> occupiedPosition, List<List<SideOfCard>> cardsOnArea) {
             return false;
         }
     }
-
-
-
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/Model/PlayGround/PlayGround.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Model/PlayGround/PlayGround.java
@@ -8,38 +8,51 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-/**
- * GameModel class
- * GameModel is the class that represents the game, it contains all the information about the game, and it's based on MVC pattern
- * It contains the list of players, the list of common play cards and common Objectives, the two deck, the game ID, the chat and the game status
- * It also contains the current player that is playing
- */
 
+/**
+ * PlayGround class
+ * PlayGround is the class that represents the game playground, it contains all the information about the game playground, and it's based on MVC pattern.
+ * It contains the list of common play cards and common Objectives, the four decks (GoldCardDeck, ResourceCardDeck, ObjectiveCardDeck, InitialCardDeck).
+ * It also contains the filepath to the resources.
+ */
 public class PlayGround implements Serializable {
 
+    // The filepath to the resources
     private String filepath = "CodexNaturalis/src/main/resources/";
-
 
     /**
      *  Decks
      */
+    // The deck of GoldCard objects
     private final Deck GoldCardDeck;
+    // The deck of ResourceCard objects
     private final Deck ResourceCardDeck;
+    // The deck of ObjectiveCard objects
     private final Deck ObjectiveCardDeck;
+    // The deck of InitialCard objects
     private final Deck InitialCardDeck;
 
     /**
-     * ist of common resourceCards, goldCard and objective card
+     * List of common resourceCards, goldCard and objective card
      */
+    // The list of common ResourceCard objects
     private final ArrayList<ResourceCard> commonResourceCards;
+    // The list of common GoldCard objects
     private final ArrayList<GoldCard> commonGoldCards;
+    // The list of common ObjectiveCard objects
     private final ArrayList<ObjectiveCard> commonObjectivesCards;
 
-
-
-
     /**
-     * Constructor
+     * Default constructor for the PlayGround class.
+     *
+     * This constructor initializes the four decks of cards (GoldCardDeck, ResourceCardDeck, ObjectiveCardDeck, InitialCardDeck)
+     * by creating new instances of the Deck class for each type of card.
+     * Each deck is then shuffled to ensure a random order of cards.
+     *
+     * It also initializes the lists of common cards (commonResourceCards, commonGoldCards, commonObjectivesCards)
+     * as new ArrayLists, ready to hold the common cards that will be used in the game.
+     *
+     * @throws IOException If there is an error reading the card data files.
      */
     public PlayGround() throws IOException {
 
@@ -58,7 +71,19 @@ public class PlayGround implements Serializable {
 
     }
 
-    /**constructor that uses filepath to retrieve json cards*/
+    /**
+     * Constructor for the PlayGround class that uses a filepath to retrieve JSON cards.
+     *
+     * This constructor initializes the four decks of cards (GoldCardDeck, ResourceCardDeck, ObjectiveCardDeck, InitialCardDeck)
+     * by creating new instances of the Deck class for each type of card, using the provided filepath to load the card data.
+     * Each deck is then shuffled to ensure a random order of cards.
+     *
+     * It also initializes the lists of common cards (commonResourceCards, commonGoldCards, commonObjectivesCards)
+     * as new ArrayLists, ready to hold the common cards that will be used in the game.
+     *
+     * @param filepath A string representing the filepath to the JSON files containing the card data.
+     * @throws IOException If there is an error reading the card data files.
+     */
     public PlayGround(String filepath) throws IOException {
         GoldCardDeck = new Deck(GoldCard.class, filepath);
         GoldCardDeck.shuffle();
@@ -72,46 +97,63 @@ public class PlayGround implements Serializable {
         commonResourceCards = new ArrayList<>();
         commonGoldCards = new ArrayList<>();
         commonObjectivesCards = new ArrayList<>();
-
     }
 
     /**
-     * @return the GoldCardDeck
+     * This method is a getter for the 'GoldCardDeck' attribute of the PlayGround class.
+     * The 'GoldCardDeck' attribute is an instance of the Deck class that represents the deck of GoldCard objects in the game.
+     * This method is used to retrieve the deck of GoldCard objects.
+     *
+     * @return An instance of the Deck class representing the deck of GoldCard objects.
      */
     public Deck getGoldCardDeck() {
         return GoldCardDeck;
     }
 
     /**
-     * @return the InitialCardDeck
+     * This method is a getter for the 'InitialCardDeck' attribute of the PlayGround class.
+     * The 'InitialCardDeck' attribute is an instance of the Deck class that represents the deck of InitialCard objects in the game.
+     * This method is used to retrieve the deck of InitialCard objects.
+     *
+     * @return An instance of the Deck class representing the deck of InitialCard objects.
      */
     public Deck getInitialCardDeck() {
         return InitialCardDeck;
     }
 
-
     /**
-     * @return the ObjectiveCardDeck
+     * This method is a getter for the 'ObjectiveCardDeck' attribute of the PlayGround class.
+     * The 'ObjectiveCardDeck' attribute is an instance of the Deck class that represents the deck of ObjectiveCard objects in the game.
+     * This method is used to retrieve the deck of ObjectiveCard objects.
+     *
+     * @return An instance of the Deck class representing the deck of ObjectiveCard objects.
      */
     public Deck getObjectiveCardDeck() {
         return ObjectiveCardDeck;
     }
 
     /**
-     * @return the ResourceCardDeck
+     * This method is a getter for the 'ResourceCardDeck' attribute of the PlayGround class.
+     * The 'ResourceCardDeck' attribute is an instance of the Deck class that represents the deck of ResourceCard objects in the game.
+     * This method is used to retrieve the deck of ResourceCard objects.
+     *
+     * @return An instance of the Deck class representing the deck of ResourceCard objects.
      */
     public Deck getResourceCardDeck() {
         return ResourceCardDeck;
     }
 
-
-
-
-
     /**
-     * Add a card to GameModel
+     * This method adds a common card to the game model.
      *
-     * @param c card
+     * The card to be added is passed as a parameter. The method uses a switch statement to check the type of the card.
+     * If the card is an instance of GoldCard, it is added to the 'commonGoldCards' list.
+     * If the card is an instance of ResourceCard, it is added to the 'commonResourceCards' list.
+     * If the card is an instance of ObjectiveCard, it is added to the 'commonObjectivesCards' list.
+     * If the card is null or of an unsupported type, an IllegalArgumentException is thrown.
+     *
+     * @param c The card to be added. This should be an instance of GoldCard, ResourceCard, or ObjectiveCard.
+     * @throws IllegalArgumentException If the card is null or of an unsupported type.
      */
     public void addCommonCard(Card c) {
 
@@ -123,43 +165,49 @@ public class PlayGround implements Serializable {
         }
     }
 
-
-
-
-
     /**
-     * @return the commonResourceCard extracted list
+     * This method is a getter for the 'commonResourceCards' attribute of the PlayGround class.
+     * The 'commonResourceCards' attribute is an ArrayList that represents the list of common ResourceCard objects in the game.
+     * This method is used to retrieve the list of common ResourceCard objects.
+     *
+     * @return An ArrayList of ResourceCard objects representing the common ResourceCard objects in the game.
      */
     public ArrayList<ResourceCard> getCommonResourceCards() {
         return commonResourceCards;
     }
 
     /**
-     * @return the commonObjectiveCard extracted list
+     * This method is a getter for the 'commonObjectivesCards' attribute of the PlayGround class.
+     * The 'commonObjectivesCards' attribute is an ArrayList that represents the list of common ObjectiveCard objects in the game.
+     * This method is used to retrieve the list of common ObjectiveCard objects.
+     *
+     * @return An ArrayList of ObjectiveCard objects representing the common ObjectiveCard objects in the game.
      */
     public ArrayList<ObjectiveCard> getCommonObjectivesCards() {
         return commonObjectivesCards;
     }
 
     /**
-     * @return the commonGoldCard extracted list
+     * This method is a getter for the 'commonGoldCards' attribute of the PlayGround class.
+     * The 'commonGoldCards' attribute is an ArrayList that represents the list of common GoldCard objects in the game.
+     * This method is used to retrieve the list of common GoldCard objects.
+     *
+     * @return An ArrayList of GoldCard objects representing the common GoldCard objects in the game.
      */
     public ArrayList<GoldCard> getCommonGoldCards() {
         return commonGoldCards;
     }
 
-
-
-
-
-
-
-
-/**
- * Method that substitutes the drawn card from the playGround
- * @param card that the player wants to draw
- * @param i index of the card. Used to substitute the card in the right position.
- * */
+    /**
+     * This method substitutes the drawn card from the playground.
+     *
+     * The method takes in two parameters: the index of the card to be replaced and the card that the player wants to draw.
+     * If the card to be drawn is an instance of GoldCard, the method removes the card from the 'commonGoldCards' list and adds a new GoldCard from the 'GoldCardDeck' at the same index.
+     * If the card to be drawn is not a GoldCard, the method assumes it is a ResourceCard. It removes the card from the 'commonResourceCards' list and adds a new ResourceCard from the 'ResourceCardDeck' at the same index.
+     *
+     * @param i The index of the card to be replaced. This is used to substitute the card in the correct position.
+     * @param card The card that the player wants to draw. This should be an instance of GoldCard or ResourceCard.
+     */
     public void drawCardFromPlayground(int i, PlayCard card){
         if (card instanceof GoldCard){
             commonGoldCards.remove(card);
@@ -170,8 +218,6 @@ public class PlayGround implements Serializable {
             commonResourceCards.add(i,(ResourceCard) ResourceCardDeck.drawCard());
         }
     }
-
-
 
 }
 
