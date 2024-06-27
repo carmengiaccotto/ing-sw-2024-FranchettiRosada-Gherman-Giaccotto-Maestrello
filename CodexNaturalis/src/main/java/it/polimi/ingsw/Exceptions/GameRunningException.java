825a928f92b499412052347fb13b134ad1775519
alessiafranchettirosada <alessia.franchettirosada@mail.polimi.it>
2024-06-27 12:35:56 +0200

    Completed JavaDoc for Exceptions.

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Exceptions/GameRunningException.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Exceptions/GameRunningException.java
@@ -1,6 +1,22 @@
 package it.polimi.ingsw.Exceptions;
 
+/**
+ * This class represents an exception that is thrown when there is an error related to a game that is currently running.
+ * It extends the RuntimeException class, meaning it is an unchecked exception.
+ * Unchecked exceptions are exceptions that can be programmatically avoided.
+ *
+ * The class has a constructor that accepts a message and a cause.
+ * The message is a string that contains a detailed description of the exception.
+ * The cause is a throwable that caused the exception to be thrown.
+ */
 public class GameRunningException extends RuntimeException {
+
+    /**
+     * This constructor creates a new GameRunningException.
+     *
+     * @param message A string containing a detailed description of the exception.
+     * @param cause A throwable that caused the exception to be thrown.
+     */
     public GameRunningException(String message, Throwable cause) {
         super(message, cause);
     }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/Exceptions/PlayerRankingException.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Exceptions/PlayerRankingException.java
@@ -1,6 +1,22 @@
 package it.polimi.ingsw.Exceptions;
 
+/**
+ * This class represents an exception that is thrown when there is an error related to player ranking.
+ * It extends the RuntimeException class, meaning it is an unchecked exception.
+ * Unchecked exceptions are exceptions that can be programmatically avoided.
+ *
+ * The class has a constructor that accepts a message and a cause.
+ * The message is a string that contains a detailed description of the exception.
+ * The cause is a throwable that caused the exception to be thrown.
+ */
 public class PlayerRankingException extends RuntimeException {
+
+    /**
+     * This constructor creates a new PlayerRankingException.
+     *
+     * @param message A string containing a detailed description of the exception.
+     * @param cause A throwable that caused the exception to be thrown.
+     */
     public PlayerRankingException(String message, Throwable cause) {
         super(message, cause);
     }
