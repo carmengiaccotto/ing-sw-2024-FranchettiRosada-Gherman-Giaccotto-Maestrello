68431625d634ee319d54b8c838d60c56993737d8
alessiafranchettirosada <alessia.franchettirosada@mail.polimi.it>
2024-06-28 03:42:00 +0200

    JavaDoc completed for JsonParser.

--- CodexNaturalis/src/main/java/it/polimi/ingsw/JsonParser/JsonParser.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/JsonParser/JsonParser.java
@@ -1,9 +1,21 @@
 package it.polimi.ingsw.JsonParser;
 
-
 import java.io.InputStream;
 
+/**
+ * This class is used to parse JSON files in the application.
+ * It provides a method to get an InputStream from a given path.
+ */
 public class JsonParser {
+
+    /**
+     * This method is used to get an InputStream from a given path.
+     * It prints the path to the console and then retrieves the InputStream associated with the path.
+     * The path is relative to the location of the JsonParser class.
+     *
+     * @param path The path to the resource.
+     * @return InputStream The InputStream associated with the given path.
+     */
     public static InputStream getStreamFromPath(String path){
         System.out.println(path);
         InputStream inputStream = JsonParser.class.getResourceAsStream(path);
